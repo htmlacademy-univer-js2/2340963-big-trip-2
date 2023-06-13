837018a55eb1dde36dbdd4fba685e5802d3f7329
Roman <romamail.ru365@gmail.com>
2023-06-13 14:29:12 +0500

    Создание и удаление данных, обратная связь

--- src/const.js
+++ src/const.js
@@ -1,26 +1,5 @@
 export const TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
 
-export const OFFERSBYTYPE = [
-  'Add luggage',
-  'Switch to comfort class',
-  'Add meal',
-  'Choose seats',
-  'Add a child safety seat'
-];
-
-export const DESTINATION_CITIES = ['Los Angeles', 'Toronto', 'Tokyo', 'Stockholm', 'Paris', 'London', 'Rome', 'Berlin', 'Copenhagen'];
-export const DESTINATION_DESCRIPTIONS = [
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  'Maecenas dapibus sed enim in ultrices.',
-  'Praesent non ex turpis.',
-  'Nunc mollis sed tortor a faucibus.',
-  'Morbi ac convallis erat, vel volutpat nisl.',
-  'Phasellus a interdum lacus. Aenean consectetur magna vel diam fringilla, at commodo sem placerat.',
-  'Etiam a pellentesque massa.',
-  'Sed sed nisi sed augue convallis suscipit in sed felis.',
-  'Aliquam erat volutpat.'
-];
-
 export const UserAction = {
   UPDATE_POINT: 'UPDATE_POINT',
   ADD_POINT: 'ADD_POINT',
@@ -37,4 +16,12 @@ export const UpdateType = {
 export const ApiServiceMethod = {
   GET: 'GET',
   PUT: 'PUT',
+  POST: 'POST',
+  DELETE: 'DELETE'
 };
+
+export const TimeLimit = {
+  LOWER_LIMIT: 350,
+  UPPER_LIMIT: 1000,
+};
+
